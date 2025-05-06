cb3d8a12111ba19e30ecf69d00ebb3997f16c7d9
Alejandro Monroy <damonroy86@ucatolica.edu.co>
2025-05-06 06:32:46 -0500

    Update connection_db.py

--- utils/connection_db.py
+++ utils/connection_db.py
@@ -3,7 +3,6 @@ from sqlmodel import SQLModel
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.orm import sessionmaker
 
-# Datos de conexión a Clever Cloud (POSTGRESQL)
 CLEVER_DB = (
     "postgresql+asyncpg://u2yvszfhd7jg7aaep3to:"
     "RfcLScfu3FaOP9o69x1Gf0NEuXCl5k@"
@@ -11,11 +10,9 @@ CLEVER_DB = (
     "50013/bay4s8hxzdohbeprhmkl"
 )
 
-# Crear el motor de la base de datos
 engine: AsyncEngine = create_async_engine(CLEVER_DB, echo=True)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
-# Inicializar la base de datos (crear tablas)
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
