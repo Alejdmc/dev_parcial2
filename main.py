3f76a0b133ff5eb595c820352d22bf2ed5b71e21
Alejandro Monroy <damonroy86ucatolica.edu.co>
2025-05-06 19:02:18 -0600

    correccion final

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 
 from fastapi import FastAPI, Depends, HTTPException
 from contextlib import asynccontextmanager
+
+from sqlalchemy.testing import db
 from sqlmodel.ext.asyncio.session import AsyncSession
 from utils.connection_db import init_db, get_session
-import operations.operations_db as crud
-from datas.models import Usuario, Estado_usuario, UsuarioCreate
-
+import operations.operations_db as op
+from datas.models import Usuario, Estado_usuario, UsuarioCreate, Tarea
+from sqlmodel import Session, select
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -16,37 +18,53 @@ app = FastAPI(lifespan=lifespan)
 
 @app.post("/usuarios/")
 async def crear(usuario: UsuarioCreate, session: AsyncSession = Depends(get_session)):
-    return await crud.crear_usuario(session, usuario)
+    return await op.crear_usuario(session, usuario)
 
 @app.get("/usuarios/")
 async def obtener_todos(session: AsyncSession = Depends(get_session)):
-    return await crud.obtener_todos(session)
+    return await op.obtener_todos(session)
 
 @app.get("/usuarios/{user_id}")
 async def obtener_uno(user_id: int, session: AsyncSession = Depends(get_session)):
-    usuario = await crud.obtener_por_id(session, user_id)
+    usuario = await op.obtener_por_id(session, user_id)
     if not usuario:
         raise HTTPException(status_code=404, detail="Usuario no encontrado")
     return usuario
 
 @app.patch("/usuarios/{user_id}/estado")
 async def actualizar(user_id: int, estado: Estado_usuario, session: AsyncSession = Depends(get_session)):
-    usuario = await crud.actualizar_estado(session, user_id, estado)
+    usuario = await op.actualizar_estado(session, user_id, estado)
     if not usuario:
         raise HTTPException(status_code=404, detail="Usuario no encontrado")
     return usuario
 
 @app.patch("/usuarios/{user_id}/premium")
 async def premium(user_id: int, session: AsyncSession = Depends(get_session)):
-    usuario = await crud.hacer_premium(session, user_id)
+    usuario = await op.hacer_premium(session, user_id)
     if not usuario:
         raise HTTPException(status_code=404, detail="Usuario no encontrado")
     return usuario
 
 @app.get("/usuarios/estado/{estado}")
 async def por_estado(estado: Estado_usuario, session: AsyncSession = Depends(get_session)):
-    return await crud.obtener_por_estado(session, estado)
+    return await op.obtener_estado(session, estado)
 
 @app.get("/usuarios/premium/activos")
 async def premium_activos(session: AsyncSession = Depends(get_session)):
-    return await crud.obtener_premium_activos(session)
+    return await op.premium_activos(session)
+
+def get_db():
+    with db.get_session() as session:
+        yield session
+
+@app.post("/tareas/", response_model=Tarea)
+def crear_tarea(tarea: Tarea, db: Session = Depends(get_db)):
+    db.add(tarea)
+    db.commit()
+    db.refresh(tarea)
+    return tarea
+
+@app.get("/tareas/", response_model=list[Tarea])
+def listar_tareas(db: Session = Depends(get_db)):
+    tareas = db.exec(select(Tarea)).all()
+    return tareas
