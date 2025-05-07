a1ee61b129aac261fb8ebf83333b5fc141d0cd50
Alejandro Monroy <damonroy86ucatolica.edu.co>
2025-05-06 20:19:57 -0600

    correccion prueba 3 deploy

--- utils/connection_db.py
+++ utils/connection_db.py
@@ -1,11 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-from sqlmodel import SQLModel
-import asyncpg
+from sqlalchemy.ext.declarative import declarative_base
 
 CLEVER_DB = (
-    "postgresql://u2yvszfhd7jg7aaep3to:"
-    "RfcLScfu3FaOP9o69x1Gf0NEuXCl5k@"
+    "postgresql+asyncpg://u2yvszfhd7jg7aaep3to:RfcLScfu3FaOP9o69x1Gf0NEuXCl5k@"
     "bay4s8hxzdohbeprhmkl-postgresql.services.clever-cloud.com:"
     "50013/bay4s8hxzdohbeprhmkl"
 )
@@ -14,13 +12,11 @@ engine = create_async_engine(CLEVER_DB, echo=True, future=True)
 
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
-async def init_db():
-    pool = await asyncpg.create_pool(dsn=CLEVER_DB)
-    async with pool.acquire() as conn:
-        pass
+Base = declarative_base()
 
+async def init_db():
     async with engine.begin() as conn:
-        await conn.run_sync(SQLModel.metadata.create_all)
+        await conn.run_sync(Base.metadata.create_all)
 
 async def get_session():
     async with async_session() as session:
